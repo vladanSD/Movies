81a15bfaa3ce9d3015d937f6ad03f4129847d7bf
Vladan <jovanovic.vladan@yahoo.com>
2017-10-14 16:18:34 +0200

    Initial commit

---
+++ app/src/main/java/com/vladan/mymovies/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vladan.mymovies.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vladan.mymovies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
