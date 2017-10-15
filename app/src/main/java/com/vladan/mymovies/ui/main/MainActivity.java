44ac039d95d3f4ef563089b60782c975eeb83303
Vladan <jovanovic.vladan@yahoo.com>
2017-10-15 18:58:57 +0200

    view pager / fragment adapter

--- app/src/main/java/com/vladan/mymovies/ui/main/MainActivity.java
+++ app/src/main/java/com/vladan/mymovies/ui/main/MainActivity.java
@@ -1,15 +1,76 @@
 package com.vladan.mymovies.ui.main;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.vladan.mymovies.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbar =  findViewById(R.id.toolbar_main);
+//        setSupportActionBar(toolbar);
+
+        initViewPager();
+    }
+
+
+
+
+    private void initViewPager() {
+        mTabLayout =  findViewById(R.id.tab_layout_main);
+        mViewPager =  findViewById(R.id.view_pager_main);
+
+        List<String> titles = new ArrayList<>();
+        titles.add(getString(R.string.tab_title_main_1));
+        titles.add(getString(R.string.tab_title_main_2));
+        titles.add(getString(R.string.tab_title_main_3));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(0)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(1)));
+        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(2)));
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new MainFragment());
+        fragments.add(new MainFragment());
+        fragments.add(new MainFragment());
+
+        mViewPager.setOffscreenPageLimit(2);
+
+        FragmentAdapter mFragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        mViewPager.setAdapter(mFragmentAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.setTabsFromPagerAdapter(mFragmentAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
