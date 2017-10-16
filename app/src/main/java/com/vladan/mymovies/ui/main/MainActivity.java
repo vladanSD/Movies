0ddbd392ebead574cc0138075885dc7cb1a37ab8
Vladan <jovanovic.vladan@yahoo.com>
2017-10-16 15:26:07 +0200

    support action bar

--- app/src/main/java/com/vladan/mymovies/ui/main/MainActivity.java
+++ app/src/main/java/com/vladan/mymovies/ui/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.vladan.mymovies.R;
 
@@ -20,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar =  findViewById(R.id.toolbar_main);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
 
         initViewPager();
     }
