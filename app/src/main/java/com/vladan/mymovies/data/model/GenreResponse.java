9508b6950cad398b549cff517afbdfed006f69be
Vladan <jovanovic.vladan@yahoo.com>
2017-11-02 12:00:43 +0100

    genre response

---
+++ app/src/main/java/com/vladan/mymovies/data/model/GenreResponse.java
@@ -0,0 +1,24 @@
+package com.vladan.mymovies.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Vladan on 11/2/2017.
+ */
+
+public class GenreResponse {
+
+    @SerializedName("results")
+    private List<Genre> genres;
+
+
+    public List<Genre> getGenres() {
+        if(genres == null){
+            return new ArrayList<>();
+        }
+        return genres;
+    }
+}
